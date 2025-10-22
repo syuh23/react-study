ae9e043d94c07058dd18cb6dd3b407521d5e5bfb
syuh23 <seonghee5070@gmail.com>
2025-10-22 18:15:31 +0900

    react setting, props study

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App';  // 여기가 App이라서 (같은 디렉토리의 App.js 파일을 import 한 것)
 import reportWebVitals from './reportWebVitals';
 
+// 이 밑의 코드들은... root라는 ID 값을 가진 태그에 해당 내용을 랜더링 하라는 뜻 !!
+// root라는 태그는 어디에 있을까? ->  public/index.html
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
